4ef1c72788c41ff0b3b765e0ccd6b0563681107a
Lorenz Bauer <lmb@cloudflare.com>
2019-06-05 10:58:19 +0100

    WIP: Remove incorrect helper

--- tools/testing/selftests/bpf/bpf_helpers.h
+++ tools/testing/selftests/bpf/bpf_helpers.h
@@ -216,9 +216,6 @@ static void *(*bpf_sk_storage_get)(void *map, struct bpf_sock *sk,
 	(void *) BPF_FUNC_sk_storage_get;
 static int (*bpf_sk_storage_delete)(void *map, struct bpf_sock *sk) =
 	(void *)BPF_FUNC_sk_storage_delete;
-static int (*bpf_skb_set_socket)(void *ctx, struct bpf_sock *sk,
-				 unsigned long long flags) =
-	(void *) BPF_FUNC_skb_set_socket;
 
 /* llvm builtin functions that eBPF C program may use to
  * emit BPF_LD_ABS and BPF_LD_IND instructions
